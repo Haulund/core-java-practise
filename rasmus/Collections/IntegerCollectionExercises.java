ea424b67a46f4371774f3e40cbe71bf51765f051
RRoager <rasmus-roager@hotmail.com>
2023-02-15 15:05:20 +0100

    Collection exercises complete

---
+++ rasmus/Collections/IntegerCollectionExercises.java
@@ -0,0 +1,81 @@
+package rasmus.Collections;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.IntStream;
+
+public class IntegerCollectionExercises {
+    public static List<Integer> numbersList = Arrays.asList(1, 2, 5, 2, -1, 2, 4, 5, 2, 4, 5, 3, 5, 2, -1, 1, 3, 1, -2, 10, 12, 11, 14, 15, 10, 12, 18, 30);
+    public static int[] numbers = {1, 2, 5, 2, -1, 2, 4, 5, 2, 4, 5, 3, 5, 2, -1, 1, 3, 1, -2, 10, 12, 11, 14, 15, 10, 12, 18, 30};
+
+    public static void main(String[] args) {
+
+        sortNumbersAscending(numbers);
+        moreThanThreeOccurrences(numbers);
+        adjacentValueHigherThanSelf(numbersList);
+        sumOfSquareOddNumbers(numbersList);
+        filterEvenNumbers(numbersList);
+        dividableByFiveAndSix(numbersList);
+    }
+
+    // 7. print x everytime a number is dividable with 6 and 5
+    private static void dividableByFiveAndSix(List<Integer> numbersList) {
+        for (int number : numbersList) {
+            if (number%5 == 0 && number%6 == 0) {
+                System.out.println("x - number " + number + " is dividable by 5 and 6");
+            } else if (number%6 == 0) {
+                System.out.println("x - number " + number + " is dividable by 6");
+            } else if (number%5 == 0) {
+                System.out.println("x - number " + number + " is dividable by 5");
+            }
+        }
+        System.out.println();
+    }
+
+    // 6. filter even numbers
+    private static void filterEvenNumbers(List<Integer> numbersList) {
+        List<Integer> evenNumbersList = numbersList.stream().filter(n -> n%2 == 0).toList();
+
+        System.out.println(evenNumbersList + "\n");
+    }
+
+    // 5. sum of squared odd numbers in list
+    private static void sumOfSquareOddNumbers(List<Integer> numbersList) {
+        Optional<Integer> optionalSum = numbersList.stream().filter(n -> n%2 != 0).map(n -> (int) Math.pow(n, 2)).reduce(Integer::sum);
+
+        optionalSum.ifPresent(sum -> System.out.println("The sum of squared odd numbers in list is: " + sum + "\n"));
+    }
+
+    // 4. List of ints -> who has an adjacent value higher than itself.
+    private static void adjacentValueHigherThanSelf(List<Integer> numbersList) {
+        // TODO Read up in mapToObj
+        List<Integer> higherThanList = IntStream.range(0, numbersList.size() - 1).filter(i -> numbersList.get(i) < numbersList.get(i + 1)).mapToObj(numbersList::get).toList();
+
+        System.out.println(higherThanList + "\n");
+    }
+
+    // 3. Array of ints -> how many int are there more than 3 times.
+    private static void moreThanThreeOccurrences(int[] numbers) {
+        HashMap<Integer, Integer> numbersAndOccurrences = new HashMap<>();
+
+        for (int number : numbers) {
+            if (!numbersAndOccurrences.containsKey(number)) {
+                numbersAndOccurrences.put(number, 1);
+            } else {
+                numbersAndOccurrences.put(number, numbersAndOccurrences.get(number) + 1);
+            }
+        }
+
+        numbersAndOccurrences.entrySet().removeIf(e -> e.getValue() < 4);
+
+        numbersAndOccurrences.forEach((key, value) -> System.out.println(key + " occurrences more than 3 times (" + value + " times)\n"));
+    }
+
+    // 1. sort an array of int ascending
+    private static void sortNumbersAscending(int[] numbers) {
+        Arrays.sort(numbers);
+        System.out.println(Arrays.toString(numbers) +  "\n");
+    }
+}
---
+++ rasmus/Collections/StringCollectionExercises.java
@@ -0,0 +1,76 @@
+package rasmus.Collections;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Optional;
+
+public class StringCollectionExercises {
+    public static void main(String[] args) {
+        String[] names = {"Bjarne", "Torben", "Hans", "Lone", "Ib", "Flemming", "Dorte", "Sigurd", "Ole"};
+
+        sortStringArray(names);
+        countLettersOfWords("Biblioteket er lukket i dag");
+        firstLetterToOccurrenceTwoTimesInWord("Biblioteket");
+        sortBasedOnFourthLetterRemoveIfLess(names);
+    }
+
+    // 1. sort an array of strings based on 4th letter, if there are less than for letters remove word from array
+    private static void sortBasedOnFourthLetterRemoveIfLess(String[] names) {
+        String[] namesLongerThanThree = Arrays.stream(names).filter(n -> n.length() > 3).toArray(String[]::new);
+
+        Arrays.sort(namesLongerThanThree, Comparator.comparingInt(s -> s.charAt(3)));
+
+        System.out.println("Sorted String array by fourth character: " + Arrays.toString(namesLongerThanThree) + "\n");
+    }
+
+    // 2. first occurrence of a letter used 2 times in a word
+    private static void firstLetterToOccurrenceTwoTimesInWord(String word) {
+        HashMap<Character, Integer> lettersAndCount = new HashMap<>();
+        char[] letters = word.toLowerCase().toCharArray();
+
+        // TODO Found on StackOverflow. Get an understanding
+//        HashMap<String, Long> lettersAndCount = (HashMap<String, Long>) Arrays.stream(word.split("")) // Stream String
+//                .map(String::toLowerCase) // All letters to lower case
+//                .filter(letter -> !letter.equals(" ")) // Remove spaces
+//                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+        for (char letter : letters) {
+            if (!lettersAndCount.containsKey(letter)) {
+                lettersAndCount.put(letter, 1);
+            } else {
+                lettersAndCount.put(letter, lettersAndCount.get(letter) + 1);
+            }
+        }
+
+        lettersAndCount.entrySet().removeIf(e -> e.getValue() != 2);
+
+        // Prints all
+//        lettersAndCount.forEach((key, value) -> System.out.println("'" + key + "' occurrences " + value + " times"));
+
+        // With help from StackOverflow
+        Optional<Character> firstLetter = lettersAndCount.keySet().stream().findFirst();
+        firstLetter.ifPresent(letter -> System.out.println("'" + letter + "' is the first letter to occurrence 2 times\n"));
+    }
+
+    // 1. Count letters of a word
+    private static void countLettersOfWords(String words) {
+        String [] allWords = words.split(" ");
+
+        for (String word : allWords) {
+            int letters = word.length();
+            System.out.println("Amount of letters in '" + word + "': " + letters);
+        }
+        System.out.println();
+    }
+
+    // 1. sort an array of Strings alphabetically
+    private static void sortStringArray(String[] names) {
+        // TODO read up on method reference operator
+        String[] sortedNames = Arrays.stream(names).sorted().toArray(String[]::new);
+
+        System.out.println("Sorted String array: " + Arrays.toString(sortedNames) + "\n");
+    }
+
+
+}
