c561e55cbb072a977a850a4ee1ac01cd03b1b34f
RRoager <rasmus-roager@hotmail.com>
2023-03-02 12:29:33 +0100

    Working on Stream Assignment

--- rasmus/MandeepExercises/StreamAssignment.java
+++ rasmus/MandeepExercises/StreamAssignment.java
@@ -2,8 +2,9 @@ package rasmus.MandeepExercises;
 
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.nio.file.Path;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Map;
 import java.util.stream.Collectors;
 
@@ -20,15 +21,45 @@ public class StreamAssignment {
      * word, it should give the following output
      */
     private static void listTop10MostUsedWords() throws IOException {
+        Map<String, Long> wordsAndCount =
+                // Read text file to a List of Strings
+                Files.readAllLines(Path.of("rasmus/MandeepExercises/stream.txt"))
+                // Stream List of Strings
+                .stream()
+                // Map to a flattened List of Strings where special characters are removed and the Strings split by whitespace and lower cased
+                .flatMap(s -> Arrays.stream(s.replaceAll("[\"?!.,:;()\\[\\]{}]", "").toLowerCase().split(" ")))
+                // Collect to a Map where the key is the String and value is the number of times the String is used
+                .collect(Collectors.groupingBy(String::valueOf, Collectors.counting()));
+
+
+        System.out.println(wordsAndCount);
+        System.out.println("Top 10 used words:");
+        // Get EntrySet of Map
+        wordsAndCount.entrySet()
+                // Stream EntrySet
+                .stream()
+                // Sort entries by the Long values and reverse the order
+                .sorted(Map.Entry.comparingByValue(Collections.reverseOrder()))
+                // Limit the output to 10 entries
+                .limit(10)
+                // Print entries
+                .forEach(e -> System.out.println("'" + e.getKey() + "' is used " + e.getValue() + " times"));
+    }
+
+
+
+
+    /*
         Map<String, Long> wordsAndCount = Files.readAllLines(Paths.get("rasmus/MandeepExercises/stream.txt"))
                 .stream()
                 .flatMap(s -> Arrays.stream(s.replaceAll("[^a-zA-Z'\\-]", " ").toLowerCase().split("\\s+")))
                 .collect(Collectors.groupingBy(String::valueOf, Collectors.counting()));
 
         System.out.println("10 most used words:");
-        wordsAndCount.entrySet().stream()
+        wordsAndCount.entrySet()
+                .stream()
                 .sorted(Map.Entry.<String, Long>comparingByValue().reversed())
                 .limit(10)
                 .forEach(e -> System.out.println("'" + e.getKey() + "' is used " + e.getValue() + " times"));
-    }
+     */
 }
